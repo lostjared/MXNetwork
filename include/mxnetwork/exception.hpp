5b78ee3a7573afd7b18d516dab752c258ac85167
Jared Bruni <jaredbruni@gmail.com>
2026-06-08 06:16:41 -0700

    updateD

--- include/mxnetwork/exception.hpp
+++ include/mxnetwork/exception.hpp
@@ -5,12 +5,10 @@
 #include <string>
 
 namespace mxnetwork {
-
     class Exception {
       public:
         Exception(const std::string &s) : txt{s} {}
         std::string text() const;
-
       protected:
         std::string txt;
     };
